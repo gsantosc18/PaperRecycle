ee2bd4c73eceb07f604b25574321fda3722243d5
Gedalias dos Santos Caldas <gsantos.c18@gmail.com>
2019-11-11 11:46:13 -0300

    Ajuste nas configurações do SMTP de envio de email

--- app/src/main/java/com/e/engapp/model/EmailConfig.java
+++ app/src/main/java/com/e/engapp/model/EmailConfig.java
@@ -1,8 +1,8 @@
 package com.e.engapp.model;
 
 public class EmailConfig {
-    public static String HOST = "smtp.gmail.com";
-    public static String PORT = "465";
-    public static String USER = "gsantos.c18@gmail.com";
+    public static String HOST = "smtp.hostinger.com.br";
+    public static String PORT = "587";
+    public static String USER = "paperrecycler@socialacademic.com.br";
     public static String PASS = "24865310@Asd";
 }
--- app/src/main/java/com/e/engapp/model/EmailSend.java
+++ app/src/main/java/com/e/engapp/model/EmailSend.java
@@ -30,7 +30,7 @@ public class EmailSend extends AsyncTask<Void,Void,Void> {
         props.put("mail.smtp.socketFactory.port", EmailConfig.PORT);
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.port", EmailConfig.PORT);
 
         session = Session.getDefaultInstance(props,
             new javax.mail.Authenticator() {
